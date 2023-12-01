f2466f001acc50a532aa76541872f40c26c610e0
A 40 years old student <a40yostudent@icloud.com>
2023-12-01 17:14:21 +0100

    another try

--- Sources/Day01.swift
+++ Sources/Day01.swift
@@ -24,14 +24,14 @@ struct Day01: AdventDay {
 		return entities.map { line in
 			let line = String(line)
 				.replacingOccurrences(of: "one", with: "1")
-				.replacingOccurrences(of: "two", with: "2")
-				.replacingOccurrences(of: "three", with: "3")
-				.replacingOccurrences(of: "four", with: "4")
-				.replacingOccurrences(of: "five", with: "5")
-				.replacingOccurrences(of: "six", with: "6")
-				.replacingOccurrences(of: "seven", with: "7")
-				.replacingOccurrences(of: "eight", with: "8")
-				.replacingOccurrences(of: "nine", with: "9")
+				// .replacingOccurrences(of: "two", with: "2")
+				// .replacingOccurrences(of: "three", with: "3")
+				// .replacingOccurrences(of: "four", with: "4")
+				// .replacingOccurrences(of: "five", with: "5")
+				// .replacingOccurrences(of: "six", with: "6")
+				// .replacingOccurrences(of: "seven", with: "7")
+				// .replacingOccurrences(of: "eight", with: "8")
+				// .replacingOccurrences(of: "nine", with: "9")
 			let numbers = line.compactMap(Int.init)
 			let first = numbers.first
 			let last = numbers.last
