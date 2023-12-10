dbcd27cc3aa5dcbddb4b5b5050176276e40ef99e
Sabino Paulicelli <a40yostudent@icloud.com>
2023-12-10 18:44:57 +0100

    Day10 in progress

--- Sources/Day10.swift
+++ Sources/Day10.swift
@@ -5,17 +5,70 @@ struct Day10: AdventDay {
 	var data: String
 	
 	// Splits input data into its component parts and convert from string.
-	var entities: [String] {
-		data.split(separator: "\n").map(String.init)
+	var entities: [[Character]] {
+		var entities = [[Character]]()
+		let lines = data.split(separator: "\n")
+		for line in lines {
+			var chars = [Character]()
+			for char in line {
+				chars.append(char)
+			}
+			entities.append(chars)
+		}
+		return entities
 	}
 	
 	// Replace this with your solution for the first part of the day's challenge.
 	func part1() -> Any {
+		var pipes = [[Pipe]]()
+		var startPipe: Pipe?
+		
+		let pipesArray = entities.map({$0.map(String.init)})/*.compactMap({ Pipe(rawValue: $0)})})*/
+		for (y, pipesLine) in pipesArray.enumerated() {
+			var pipeLine = [Pipe]()
+			for (x, pipeChar) in pipesLine.enumerated() {
+				let pipe = Pipe(type: PipeType(rawValue: pipeChar)!, x: x, y: y)
+				pipeLine.append(pipe)
+				if pipe.type == ._s {
+					startPipe = pipe
+				}
+			}
+			pipes.append(pipeLine)
+		}
+		
+		guard let startPipe else { fatalError("start pipe not found") }
+		
+		print(pipes.debugDescription, startPipe)
+		
 		return 0
 	}
 	
 	// Replace this with your solution for the second part of the day's challenge.
 	func part2() -> Any {
 		return 0
+	}
+}
+
+extension Day10 {
+	fileprivate enum PipeType: String {
+		case _v = "|", _h = "-", _l = "L", _j = "J", _7 = "7", _f = "F", _g = ".", _s = "S"
+	}
+	
+	fileprivate struct Pipe {
+		let type: PipeType
+		let x: Int
+		let y: Int
+	}
+}
+
+extension Day10.PipeType: CustomDebugStringConvertible {
+	var debugDescription: String {
+		return self.rawValue
+	}
+}
+
+extension Day10.Pipe: CustomDebugStringConvertible {
+	var debugDescription: String {
+		return "\(self.type.debugDescription), x: \(self.x.description), y: \(self.y.description)"
 	}
 }
--- Tests/Day10.swift
+++ Tests/Day10.swift
@@ -6,16 +6,34 @@ import XCTest
 // pass.
 final class Day10Tests: XCTestCase {
 	// Smoke test data provided in the challenge question
-	let testData = """
+	let testData = ("""
+	-L|F7
+	7S-7|
+	L|7||
+	-L-J|
+	L|-JF
+	""",
 	"""
+	7-F7-
+	.FJ|7
+	SJLL7
+	|F--J
+	LJ.LJ
+	""") // 4, 8
 	
 	func testPart1() throws {
-		let challenge = Day10(data: testData)
-		XCTAssertEqual(String(describing: challenge.part1()), "0")
+		let challenge0 = Day10(data: testData.0)
+		XCTAssertEqual(String(describing: challenge0.part1()), "0")
+		
+		let challenge1 = Day10(data: testData.1)
+		XCTAssertEqual(String(describing: challenge1.part1()), "0")
 	}
 	
 	func testPart2() throws {
-		let challenge = Day10(data: testData)
-		XCTAssertEqual(String(describing: challenge.part2()), "0")
+		let challenge0 = Day10(data: testData.0)
+		XCTAssertEqual(String(describing: challenge0.part2()), "0")
+		
+		let challenge1 = Day10(data: testData.1)
+		XCTAssertEqual(String(describing: challenge1.part1()), "0")
 	}
 }
